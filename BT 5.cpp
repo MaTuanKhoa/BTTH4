ebcb92db953ab68dff4380d460ca2aaaaed51ec4
Ma Tuấn Khoa <makhoa05@gmail.com>
2023-10-19 07:27:43 +0700

    Update BT 5.cpp

--- BT 5.cpp
+++ BT 5.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int n, sum = 0;
-    cout << "Nhap so: ";
+    int n;
+    double sum = 0.0;
+    cout << "Enter a positive integer: ";
     cin >> n;
-    for (int i = 1; i <= n; i++)
-        sum += i;
-    cout << "Tong cac so tu 1 den " << n << " la: " << sum;
+    for (int i = 1; i <= n; ++i) {
+        sum += 1.0 / (1 + i * i);
+    }
+    cout << "The sum is " << sum << endl;
     return 0;
 }
-
